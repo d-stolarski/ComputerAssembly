0d3b3068eda7563bd9b0e20777d0f0f52aa364ff
Daniel Stolarski <d.stolarski@o2.pl>
2017-12-01 08:32:50 +0100

    Second Commit - ComputerTest class created and tested

--- src/Memory.java
+++ src/Memory.java
@@ -1,2 +1,14 @@
 public class Memory {
+
+    String producer;
+    String model;
+    int value;
+    double clocking;
+
+    Memory(String producer, String model, int value, double clocking) {
+        this.producer = producer;
+        this.model = model;
+        this.value = value;
+        this.clocking = clocking;
+    }
 }
--- src/Procesor.java
+++ src/Procesor.java
@@ -1,3 +1,12 @@
 public class Procesor {
 
+    String producer;
+    String model;
+    double clocking;
+
+    Procesor(String producer, String model, double clocking) {
+        this.producer = producer;
+        this.model = model;
+        this.clocking = clocking;
+    }
 }
